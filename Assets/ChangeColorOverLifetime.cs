8fca6248e911f111aada1d09d49e40d0771c889a
Luis Herranz Mañas <luishmanas@gmail.com>
2017-02-07 18:25:00 +0000

    Aesthetic Fix
    The floor is making lickr happen in android
    Fountain has new particles and now renders properly.

---
+++ Assets/ChangeColorOverLifetime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorOverLifetime : MonoBehaviour {
+
+    void Start()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        var col = ps.colorOverLifetime;
+        col.enabled = true;
+
+        Gradient grad = new Gradient();
+        grad.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.blue, 0.0f), new GradientColorKey(Color.red, 1.0f) }, new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(0.0f, 1.0f) });
+
+        col.color = grad;
+    }
+}
